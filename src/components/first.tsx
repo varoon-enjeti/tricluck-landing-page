b183f5fcb3677dc3d244c3b155edf302da899a2c
varoon-enjeti <varoon.enjeti@gmail.com>
2023-06-21 12:01:41 -0500

    testing vercel

--- src/components/first.tsx
+++ src/components/first.tsx
@@ -7,6 +7,7 @@ export default function First() {
             <p>please use the first.tsx file as a reference for formatting new TSX files</p>
             <p>to test your components in live time run 'npm start' in your terminal, and your browser will update on save</p>
             <p>finally, remember to import your components in App.js, and again, follow the same formatting as First</p>
+            <p className="text-purple-600">testing vercel</p>
             <p className='text-blue-600'>code blooded</p>
         </div>
     );
