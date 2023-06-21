98c44149619990c66e04b1b9e82644f85867760c
varoon-enjeti <varoon.enjeti@gmail.com>
2023-06-21 11:31:49 -0500

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>White. A blank page or canvas. So many possibilities.</div>
-        <br />
-        <div>If you're seeing this, you repo is set up correctly</div>
+        <div className="p-4 font-bold underline">White. A blank page or canvas. So many possibilities.</div>
+        <div className='p-4'>If you're seeing this, you repo is set up correctly</div>
       </header>
     </div>
   );
