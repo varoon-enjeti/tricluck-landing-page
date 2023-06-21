67c403cfcb8e54e3f8f7a34613760f5bb03af081
varoon-enjeti <varoon.enjeti@gmail.com>
2023-06-21 12:22:25 -0500

    instructions

--- src/components/first.tsx
+++ src/components/first.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function First() {
     return (
         <div className="p-4">
-            <p className="text-red-600">navigate to the src/components directory to begin adding your tsx files for each part of the page</p>
+            <p className="text-red-600">navigate to the src/components directory to begin adding your tsx files</p>
             <p>please use the first.tsx file as a reference for formatting new TSX files</p>
             <p>to test your components in live time run 'npm start' in your terminal, and your browser will update on save</p>
             <p>finally, remember to import your components in App.js, and again, follow the same formatting as First</p>
