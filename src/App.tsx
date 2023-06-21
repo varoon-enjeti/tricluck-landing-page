fc08b95a152b3b0db4cfedec428858c2579f7546
varoon-enjeti <varoon.enjeti@gmail.com>
2023-06-21 11:42:59 -0500

    setup stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import First from './components/first';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div className="p-4 font-bold underline">White. A blank page or canvas. So many possibilities.</div>
-        <div className='p-4'>If you're seeing this, you repo is set up correctly</div>
+        <div className='p-4'>If you're seeing this, your repo is set up correctly</div>
+        <First />
       </header>
     </div>
   );
---
+++ src/components/first.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function First() {
+    return (
+        <div className="p-4">
+            <p className="text-red-600">navigate to the src/components directory to begin adding your tsx files for each part of the page</p>
+            <p>please use the first.tsx file as a reference for formatting new TSX files</p>
+            <p>to test your components in live time run 'npm start' in your terminal, and your browser will update on save</p>
+            <p>finally, remember to import your components in App.js, and again, follow the same formatting as First</p>
+            <p className='text-blue-600'>code blooded</p>
+        </div>
+    );
+}
