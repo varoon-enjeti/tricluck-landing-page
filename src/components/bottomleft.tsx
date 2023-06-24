7b13bdeaaeb21088cf1558669fd3387f257c2482
SirBillyBob <aaron.husun@gmail.com>
2023-06-24 00:33:31 -0400

    Finished Bottom Right
    fixed default functions, submit button no functionality

--- src/components/bottomleft.tsx
+++ src/components/bottomleft.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import phone from './img/image.png'
 
-export default function First() {
+export default function BottomLeft() {
     return (
         <div className="xl:w-7/12 xl:h-1/2">
             <img src={phone} alt ="phone"/>
--- src/components/bottomright.tsx
+++ src/components/bottomright.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 
-export default function First() {
-    return (
-        <div className="p-4">
-            <p className="text-red-600">navigate to the src/components directory to begin adding your tsx files</p>
-            <p>please use the first.tsx file as a reference for formatting new TSX files</p>
-            <p>to test your components in live time run 'npm start' in your terminal, and your browser will update on save</p>
-            <p>finally, remember to import your components in App.js, and again, follow the same formatting as First</p>
-            <p className="text-purple-600">testing vercel</p>
-            <p className='text-blue-600'>code blooded</p>
+export default function BottomRight() {
+  const [userInput, setUserInput] = useState('');
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUserInput(event.target.value);
+};
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        // Perform any submission logic here
+      
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', height: '100vh' }}>
+      <h1 style={{ fontSize: '4rem', color: 'black' }}>Learn with Us!</h1>
+      <h1 style={{ fontSize: '3rem', color: 'black' }}>Join the Waitlist</h1>
+      <form onSubmit={handleSubmit}>
+        <div style={{ display: 'flex', alignItems: 'center',justifyContent: 'space-between', margin: '10px',
+              paddingLeft: '10px',
+              paddingRight: '0',
+              border: '2px solid #ccc',
+              borderRadius: '20px',
+              backgroundColor: 'white',
+              width: '420px',
+              height: '70px', }}>
+          <input
+            type="text"
+            value={userInput}
+            onChange={handleInputChange}
+            placeholder="Enter your email"
+            style={{
+              fontSize: '1.25rem',
+            }}
+          />
+          <button type="submit" style={{ padding: '10px 10px', fontSize: '1.5rem', backgroundColor: '#4679e8', color: 'white', border: 'none', borderRadius: '20px', height:'66px' }}>
+            Submit
+          </button>
         </div>
-    );
+      </form>
+    </div>
+  );
 }
