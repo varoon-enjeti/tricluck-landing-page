4ffccfadfbaf75a9aa6cf1134d1ed55eedc154f1
SirBillyBob <aaron.husun@gmail.com>
2023-06-24 00:48:30 -0400

    Mobile Accomodated
    1200px width to switch views

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import logo from './logo.svg';
-import First from './components/first';
 import BottomRight from './components/bottomright';
 import BottomLeft from './components/bottomleft';
 
-
-
 function App() {
+  const isMobile = useMediaQuery({ maxWidth: 1200 }); // Define the breakpoint for mobile view
+
   return (
     <div className="xl:flex font-Ubuntu">
-      <BottomLeft />
-      <BottomRight />
+      <div className="xl:w-7/12 xl:h-1/2">
+        {isMobile ? <BottomRight /> : <BottomLeft />}
+      </div>
+      <div className="xl:w-5/12 xl:h-1/2">
+        {isMobile ? <BottomLeft /> : <BottomRight />}
+      </div>
     </div>
   );
 }
--- src/components/bottomleft.tsx
+++ src/components/bottomleft.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import phone from './img/image.png'
+import phone from './img/image.png';
 
 export default function BottomLeft() {
-    return (
-        <div className="xl:w-7/12 xl:h-1/2">
-            <img src={phone} alt ="phone"/>
-        </div>
-    );
+  return (
+    <div className="xl:w-7/12 xl:h-1/2 flex justify-center items-center">
+      <img src={phone} alt="phone" />
+    </div>
+  );
 }
