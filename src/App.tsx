109771b21f7013cc6351e488350431c8abad1724
varoon-enjeti <varoon.enjeti@gmail.com>
2023-06-27 09:36:35 -0700

    merged branches

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
-import First from './components/first';
+import TopLeft from './components/topleft';
+import TopRight from './components/topright';
+import BottomRight from './components/bottomright';
+import BottomLeft from './components/bottomleft';
+import { useMediaQuery } from 'react-responsive';
 
 function App() {
   const isMobile = useMediaQuery({ maxWidth: 1200 }); // Define the breakpoint for mobile view
 
   return (
-    <div className="xl:flex font-Ubuntu">
-      <TopLeft />
-      <TopRight />
-      <div className="xl:w-7/12 xl:h-1/2">
-        {isMobile ? <BottomRight /> : <BottomLeft />}
+    <div className="flex-col">
+      <div className="xl:flex font-Ubuntu">
+        <TopLeft />
+        <TopRight />
       </div>
-      <div className="xl:w-5/12 xl:h-1/2">
-        {isMobile ? <BottomLeft /> : <BottomRight />}
+      <div className="xl:flex font-Ubuntu">
+        <div className="xl:w-7/12 xl:h-1/2">
+            {isMobile ? <BottomRight /> : <BottomLeft />}
+          </div>
+        <div className="xl:flex font-Ubuntu">
+          <div className="xl:w-5/12 xl:h-1/2">
+            {isMobile ? <BottomLeft /> : <BottomRight />}
+          </div>
+        </div>
       </div>
     </div>
   );
