0eaa56f5729b96eb31caece8ca4200d15bebfa2c
SirBillyBob <aaron.husun@gmail.com>
2023-06-29 01:15:14 -0400

    Shifting to tailwind
    changed to tailwindcss, submit button changes

--- src/components/bottomright.tsx
+++ src/components/bottomright.tsx
@@ -1,46 +1,39 @@
 import React, { useState, ChangeEvent, FormEvent } from 'react';
 
-export default function BottomRight() {
+const BottomRight = () => {
   const [userInput, setUserInput] = useState('');
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setUserInput(event.target.value);
-};
+  };
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        // Perform any submission logic here
-      
+    event.preventDefault();
+    // Perform any submission logic here
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', height: '100vh', margin: 'auto', }}>
-      <h1 style={{ fontSize: '4rem', color: 'black' }}>Learn with Us!</h1>
-      <h1 style={{ fontSize: '3rem', color: 'black' }}>Join the Waitlist</h1>
-      <form onSubmit={handleSubmit}>
-        <div style={{ display: 'flex', alignItems: 'center',justifyContent: 'space-between', margin: '10px',
-              paddingLeft: '10px',
-              paddingRight: '0',
-              border: '2px solid #ccc',
-              borderRadius: '20px',
-              backgroundColor: 'white',
-              width: '420px',
-              height: '70px', }}>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl text-black">Learn with Us!</h1>
+      <h1 className="text-3xl text-black">Join the Waitlist</h1>
+      <form onSubmit={handleSubmit} className="mt-10">
+        <div className="flex items-center justify-between w-96 p-2 border-2 border-gray-300 rounded-lg bg-white">
           <input
             type="text"
             value={userInput}
             onChange={handleInputChange}
             placeholder="Enter your email"
-            style={{
-              fontSize: '1.25rem',
-              outline: 'none',
-            }}
+            className="w-3/4 px-2 text-lg outline-none"
           />
-          <button type="submit" style={{ padding: '10px 10px', fontSize: '1.3rem', backgroundColor: '#4679e8', color: 'white', border: 'none', borderRadius: '20px', height:'66px' }}>
+          <button
+            type="submit"
+            className="px-4 py-2 text-lg bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-all duration-300 hover:px-6 hover:py-3"
+          >
             Submit
           </button>
         </div>
       </form>
     </div>
   );
-}
+};
+export default BottomRight;
