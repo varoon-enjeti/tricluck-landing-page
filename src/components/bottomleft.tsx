d6c6131608f7a28794f66f27886217dcbb8c9408
SirBillyBob <aaron.husun@gmail.com>
2023-07-01 02:48:45 -0400

    Updated bottom left
    Changed to Tailwind CSS

--- src/components/bottomleft.tsx
+++ src/components/bottomleft.tsx
@@ -3,14 +3,12 @@ import phone from './img/image.png';
 
 export default function BottomLeft() {
   return (
-    <div className="xl:w-7/12 xl:h-1/2 flex justify-center items-center">
-      <img src={phone} alt="phone" style={{
-                    width: '50%',
-                    height: 'auto',
-                    display: 'block',
-                    margin: 'auto',
-
-                }}/>
+    <div className="flex justify-center items-center h-screen">
+      <img
+        src={phone}
+        alt="phone"
+        className="w-1/2 h-auto"
+      />
     </div>
   );
 }
